60363eb5a659a9c92c6a944148d5550e41fa204d
Ishmeet Singh <ishmeet.1136@gmail.com>
2016-09-21 09:24:00 +0530

    Changed files according to the review.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -9,7 +9,7 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
@@ -29,7 +29,7 @@ import com.example.xyzreader.data.UpdaterService;
  * touched, lead to a {@link ArticleDetailActivity} representing item details. On tablets, the
  * activity presents a grid of items as cards.
  */
-public class ArticleListActivity extends ActionBarActivity implements
+public class ArticleListActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
 
     private Toolbar mToolbar;
